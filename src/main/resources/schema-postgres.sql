b2f81137d1171c6dc0053a92a36579418b5691cd
vanaraen <vodkachoke65@gmail.com>
2022-08-19 08:41:53 +0000

    tryout to init data 4# - move to .gitpod.yml instead

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,2 +1,2 @@
-DROP TABLE IF EXISTS cities;
-CREATE TABLE cities(id serial PRIMARY KEY, name VARCHAR(255), population integer);
+-- DROP TABLE IF EXISTS cities;
+-- CREATE TABLE cities(id serial PRIMARY KEY, name VARCHAR(255), population integer);
