e10fbcf4f34a0fc86edc550ed7e137cdb9a4ab17
vanaraen <vodkachoke65@gmail.com>
2022-08-19 05:54:13 +0000

    Automate startup tryout #1

--- src/main/java/com/example/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/example/springdatajpa/SpringDataJpaApplication.java
@@ -8,6 +8,7 @@ public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJpaApplication.class, args);
+		System.out.println("hello World!");
 	}
 
 }
