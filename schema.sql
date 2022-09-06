d4084d4ed4f1cf32db8b243ecba433ab4895224a
Van Araen <79781208+vanaraen1111@users.noreply.github.com>
2022-09-06 16:56:57 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,7 @@
-DROP TABLE IF EXISTS cities;
-CREATE TABLE cities(id serial PRIMARY KEY, name VARCHAR(255), population integer);
+DROP TABLE IF EXISTS "Cities";
+
+CREATE TABLE "Cities" (
+	"Id" serial PRIMARY KEY,
+	"Name" int4 NULL,
+	"Population" int4 NULL
+)
