44b4f571a0162679a10bd249ab121c83974fd418
vanaraen <vodkachoke65@gmail.com>
2022-09-13 02:46:00 +0000

    remove unused, add logging

--- src/main/java/com/example/springdatajpa/controller/CityController.java
+++ src/main/java/com/example/springdatajpa/controller/CityController.java
@@ -1,5 +1,7 @@
 package com.example.springdatajpa.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,15 @@ import com.example.springdatajpa.service.CitiesServices;
 
 @RestController
 public class CityController {
+
+private final Logger logger = LoggerFactory.getLogger(CityController.class);
+
     @Autowired
     private CitiesServices cityServices;
     
     @GetMapping("/api/cities/all")
     public String getAllCities() {
+        logger.debug("/api/cities/all => CALLED");
         return cityServices.list().toString();
     }
 }
