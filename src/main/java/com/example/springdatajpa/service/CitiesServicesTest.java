a105754cc498ab4e20c2ef80753a4dcc948cd2ef
vanaraen <vodkachoke65@gmail.com>
2023-06-21 07:51:04 +0000

    update junit dependency

--- src/main/java/com/example/springdatajpa/service/CitiesServicesTest.java
+++
@@ -1,5 +0,0 @@
-package com.example.springdatajpa.service;
-
-public class CitiesServicesTest {
-    
-}
---
+++ src/test/java/com/example/springdatajpa/CitiesServicesTest.java
@@ -0,0 +1,6 @@
+package com.example.springdatajpa;
+
+@Test
+public class CitiesServicesTest {
+    
+}
