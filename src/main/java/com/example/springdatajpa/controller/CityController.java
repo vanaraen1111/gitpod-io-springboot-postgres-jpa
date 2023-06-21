9e2fc9db14d7a1f7aaa8b4b9c19bc5c684b7d2e0
vanaraen <vodkachoke65@gmail.com>
2023-06-21 10:24:31 +0000

    fix import

--- src/main/java/com/example/springdatajpa/controller/CityController.java
+++ src/main/java/com/example/springdatajpa/controller/CityController.java
@@ -2,7 +2,6 @@ package com.example.springdatajpa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatus$Series;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus.Series;
 import org.springframework.web.bind.annotation.GetMapping;
