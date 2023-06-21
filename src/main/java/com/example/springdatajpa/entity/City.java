a0d9750bfec9f1fc5bae979ea9fb1cc622f6c02b
vanaraen <vodkachoke65@gmail.com>
2023-06-21 04:05:58 +0000

    Revert "Change id data type"
    This reverts commit d033f0a906d7bc313b978e24304a3f31bcad678b.

--- src/main/java/com/example/springdatajpa/entity/City.java
+++ src/main/java/com/example/springdatajpa/entity/City.java
@@ -15,7 +15,7 @@ public class City {
     @GeneratedValue(strategy = GenerationType.AUTO)
 
     @Column(name="`Id`")
-    private Integer id;
+    private Long id;
 
     @Column(name="`Name`")
     private String name;
@@ -26,11 +26,11 @@ public class City {
     public City() {
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
