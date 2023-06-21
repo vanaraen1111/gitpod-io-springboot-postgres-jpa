a3325a8e7a1139d5e79d70786902c114a370d36e
vanaraen <vodkachoke65@gmail.com>
2023-06-21 02:49:43 +0000

    fix request param

--- src/main/java/com/example/springdatajpa/controller/CityController.java
+++ src/main/java/com/example/springdatajpa/controller/CityController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springdatajpa.model.BaseResponse;
@@ -22,10 +23,10 @@ public class CityController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @GetMapping("api/cities/{id}")
-    public ResponseEntity<BaseResponse> getCitiesById(String id) {
+    @GetMapping("api/cities")
+    public ResponseEntity<BaseResponse> getCitiesById(@RequestParam(value = "id") Integer id) {
         BaseResponse response = new BaseResponse();
-        response.setResponseData(citiesServices.getCityById(Integer.valueOf(id)));
+        response.setResponseData(citiesServices.getCityById(id));
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
